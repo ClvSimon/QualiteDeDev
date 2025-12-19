c7ef938818a29fbe06d433e0d7c4f059836acd7b
ClvSimon <s.clavel.82@gmail.com>
2025-12-19 13:07:54 +0100

    ajout harry potter

---
+++ TP1/mon-projet/src/test/java/com/exemple/HarryPotterTest.java
@@ -0,0 +1,15 @@
+package com.exemple;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for GameOfLife.
+ */
+public class HarryPotterTest {
+
+    @Test
+    void testlivre() {
+
+    }
+}
